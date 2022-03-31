f0cd30223b19a967e87e00779f6e51cd643fea3f
seockhun <hun00719@gmail.com>
2022-03-31 20:04:45 +0900

    [UPDATE] INIT

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  swcMinify: true,
   reactStrictMode: true,
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
 }
---
+++ styles/color.ts
@@ -0,0 +1,6 @@
+export const COLOR = {
+    main: "#F6AE21",
+    second: "D23C61",
+    black: "#000000",
+    white: "#fff",
+}
