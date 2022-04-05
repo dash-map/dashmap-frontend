ca1baacea7161cc288b038f104e70ee8eb14de3d
seockhun <hun00719@gmail.com>
2022-04-05 20:20:41 +0900

    [UPDATE] status bar

---
+++ pages/main/question/qustion.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Question = () => {
+    return(
+        <Wrapper></Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    width: 50%;
+    height: 480px;
+    border: 1px solid #ececec;
+    border-radius: 7px;
+    box-shadow: 0px 0px 10px #C8C8C8;
+`
+
+export default Question;
