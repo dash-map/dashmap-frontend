bb34c857f41f79bf81b47ee340554b4d05e38ae4
seockhun <hun00719@gmail.com>
2022-04-20 23:31:30 +0900

    [UPDATE] redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import configureStore from '../reducers/configureStore';
+
+const store = configureStore();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  ) 
 }
+
+MyApp.getInitalProps = async (appContext: AppContext) => {
+  const appProps: any = await MyApp.getInitalProps(appContext);
+
+  return {...appProps}
+}
+
 export default MyApp
