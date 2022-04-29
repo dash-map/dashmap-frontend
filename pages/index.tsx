7aaf61ff68dd2d8e4a4557c4ab843c2c40ac5813
seockhun <hun00719@gmail.com>
2022-04-29 12:07:44 +0900

    [UPDATE] .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,12 @@
 import type { NextPage } from 'next'
-import styled from "styled-components";
 
 const Home: NextPage = () => {
+
   return (
-    <Wrapper>
-      
-    </Wrapper>    
+    <div></div>
   )
 }
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-image: url('https://dash-map.s3.ap-northeast-2.amazonaws.com/frontend/background.png');
-  background-size: 100% 100vh;
-`
+
 
 export default Home;
