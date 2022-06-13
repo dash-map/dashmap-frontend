1e512ad4798b695f06209739b79d6e5e04c0ecdc
seockhun <hun00719@gmail.com>
2022-06-14 08:42:09 +0900

    [UPDATE] lecture

--- pages/main/leccture/lecture.tsx
+++ pages/main/leccture/lecture.tsx
@@ -25,6 +25,13 @@ const Lecture = () => {
 
     return(
         <Wrapper>
+            {
+                state.map((e: any, index: number) => {
+                    return(
+                        e[1]
+                    )
+                })
+            }
             <iframe width="560px" height="315px" src="https://www.youtube.com/embed/edWbHp_k_9Y" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/DHPeeEvDbdo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/BUTP-YsD3nM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
