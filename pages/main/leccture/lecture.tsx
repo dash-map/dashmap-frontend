0c468c526a20d020b92c2ee6c5a918e0f38520af
seockhun <hun00719@gmail.com>
2022-06-16 11:32:20 +0900

    [UPDATE] .

--- pages/main/leccture/lecture.tsx
+++ pages/main/leccture/lecture.tsx
@@ -4,12 +4,12 @@ import { requestWithAccessToken } from "../../../utils/axios/axios";
 import axios from "axios";
 
 interface Props {
-    rec_1: [];
-    rec_2: [];
-    rec_3: [];
-    rec_4: [];
-    rec_5: [];
-    rec_6: [];
+    rec_1: any;
+    rec_2: any;
+    rec_3: any;
+    rec_4: any;
+    rec_5: any;
+    rec_6: any;
 }
 
 const Lecture = () => {
@@ -35,7 +35,7 @@ const Lecture = () => {
                 rec_5: res.data.recommend_5,
                 rec_6: res.data.recommend_6,
             });
-            console.log(res.data)
+            console.log(res)
         }).catch((err) => {
             console.log(err);
         })
@@ -43,21 +43,28 @@ const Lecture = () => {
 
     useEffect(() => {
         console.log(state)
-        console.log(state.rec_1[1].url)
-    }, [state])
+    }, [state]);
+
+    const watch = (e) => {
+        axios.post(`${URL}/watch-record/${localStorage.getItem("userId")}/${e}?rating=${5}`).then((res) => {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
 
 
     return(
         <Wrapper>
             {
-                state &&
+                state.rec_1[1] &&
                 <>
-                    <iframe width="560px" height="315px" src={`${state.rec_1[1].url}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/DHPeeEvDbdo" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/BUTP-YsD3nM" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/cEUIhLOgWZI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/IDVnZPjRCYg" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/9XjpfW8U3P4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <iframe onClick={() => {watch(state.rec_1[0].id)}} width="560px" height="315px" src={state.rec_1[1].url} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <iframe width="560" height="315" src={state.rec_2[1].url} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <iframe width="560" height="315" src={state.rec_3[1].url} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <iframe width="560" height="315" src={state.rec_4[1].url} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <iframe width="560" height="315" src={state.rec_5[1].url} allowFullScreen></iframe>
+                        <iframe width="560" height="315" src={state.rec_6[1].url} allowFullScreen></iframe>
                 </> 
             }
             
